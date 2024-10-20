4c9313b19f8d568987fc3eaadb350ff4eb1ea467
Gastón Bertolani <gaston.bertolani@gmail.com>
2024-10-20 17:22:42 -0300

    [FIX]relative path

--- sw.js
+++ sw.js
@@ -1,16 +1,16 @@
 const CACHE_NAME = 'pwa-cache-v1';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/static/vendor/joy/joy.min.js',
-    '/static/css/styles.css',
-    '/static/js/app.js',
-    '/static/js/2Dmap.js',
-    '/static/js/settings.js',
-    '/manifest.json',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png',
-    '/icons/icon-1024x1024.png',
+    './',
+    './index.html',
+    './static/vendor/joy/joy.min.js',
+    './static/css/styles.css',
+    './static/js/app.js',
+    './static/js/2Dmap.js',
+    './static/js/settings.js',
+    './manifest.json',
+    './icons/icon-192x192.png',
+    './icons/icon-512x512.png',
+    './icons/icon-1024x1024.png',
 ];
 
 self.addEventListener('install', event => {
